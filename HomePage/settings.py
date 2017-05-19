a80f7f4986a98c8698d3481cd873bb8492c78d83
jacekkarnasiewicz <Swhway@wp.pl>
2017-05-19 18:15:40 +0200

    heroku settings

--- HomePage/settings.py
+++ HomePage/settings.py
@@ -157,7 +157,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'asgi_redis.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('localhost', 6379)],
+            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
         'ROUTING': 'HomePage.routing.channel_routing',
     }
