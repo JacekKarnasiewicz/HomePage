1ebb95c1cce09b0f7e1d450f8e7d7a2725cadc9f
jacekkarnasiewicz <Swhway@wp.pl>
2017-05-21 00:11:17 +0200

    Displaying menu, settings changes and clearing-up icons

--- HomePage/settings.py
+++ HomePage/settings.py
@@ -34,6 +34,8 @@ DEBUG = True
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+CSRF_COOKIE_SECURE = True
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
