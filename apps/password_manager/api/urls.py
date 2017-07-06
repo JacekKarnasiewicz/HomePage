192c875b470f9b34ed5e29b4f75a765ac05e3fd5
jacekkarnasiewicz <Swhway@wp.pl>
2017-07-06 17:55:06 +0200

    urls regexp fix

--- apps/password_manager/api/urls.py
+++ apps/password_manager/api/urls.py
@@ -10,8 +10,8 @@ from .views import (
 
 urlpatterns = [
 	url(r'^$', PasswordManagerListAPIView.as_view(), name='list'),
-	url(r'^retrieve/(?P<site_name>[\w]+)/$', PasswordManagerRetrieveAPIView.as_view(), name='retrieve'),
+	url(r'^retrieve/(?P<site_name>[\w\s]+)/$', PasswordManagerRetrieveAPIView.as_view(), name='retrieve'),
 	url(r'^create/$', PasswordManagerCreateAPIView.as_view(), name='create'),	
-	url(r'^update/(?P<site_name>[\w]+)/$', PasswordManagerUpdateAPIView.as_view(), name='update'),
-	url(r'^destroy/(?P<site_name>[\w]+)/$', PasswordManagerDestroyAPIView.as_view(), name='destroy'),
+	url(r'^update/(?P<site_name>[\w\s]+)/$', PasswordManagerUpdateAPIView.as_view(), name='update'),
+	url(r'^destroy/(?P<site_name>[\w\s]+)/$', PasswordManagerDestroyAPIView.as_view(), name='destroy'),
 ]
